63334852f12d6ed1b708be0cab2a25b286aa4ad3
min <m2insapp@gmail.com>
2024-09-26 17:45:27 +0900

    feat: day_03_클래스와 객체

---
+++ hdm/JAVA-클래스와_객체/객체지향/src/main/java/org/example/FuncitonTest/FunctionTest.java
@@ -0,0 +1,11 @@
+package org.example.FuncitonTest;
+
+public class FunctionTest {
+    public static void main(String[] args) {
+//        System.out.println("아침에 일어난다");
+
+
+        //함수로 관련된 문장을 하나로 묶을 수 있음.
+
+    }
+}
---
+++ hdm/JAVA-클래스와_객체/객체지향/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.Person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person yang = new Person();
+
+        yang.name = "Yang";
+        yang.age = 45;
+        yang.hobby = "YouTube";
+
+        Person hong = new Person();
+        hong.name = "Hong";
+        hong.age = 25;
+        hong.hobby = "Golf";
+
+    }
+
+}

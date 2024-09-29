c5d2389c7780248bd8768c941faa23d25cd433bf
mins <m2insapp@gmail.com>
2024-09-29 16:03:52 +0900

    feat 05 상속

---
+++ hdm/JAVA-상속/codes/Inheritance/src/main/java/org/example/Testinheritance1/Test.java
@@ -0,0 +1,9 @@
+package org.example.Testinheritance1;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        Studnet st = new Studnet();
+    }
+}
